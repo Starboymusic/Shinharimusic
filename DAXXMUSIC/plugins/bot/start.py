263f894bed0a1c40fd8d20197741c61097448f4e
Starboymusic <visionm59@gmail.com>
2024-09-13 18:50:58 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -29,9 +29,9 @@ from strings import get_string
 #--------------------------
 
 NEXI_VID = [
-"https://imgur.com/tFJhi4K",
-"https://imgur.com/a/xY5q6nw",
-"https://imgur.com/a/bmYVzbH",
+"https://telegra.ph/file/1a3c152717eb9d2e94dc2.mp4",
+"https://graph.org/file/ba7699c28dab379b518ca.mp4",
+"https://graph.org/file/83ebf52e8bbf138620de7.mp4",
 "https://graph.org/file/82fd67aa56eb1b299e08d.mp4",
 "https://graph.org/file/318eac81e3d4667edcb77.mp4",
 "https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
